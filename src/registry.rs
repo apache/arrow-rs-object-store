9c72689beaee1e00e7e680af9bdafc95ad52f2a7
Chris Riccomini <criccomini@apache.org>
2025-05-06 10:09:34 -0700

    Strip `ObjectStoreUrl` and use  instead of

--- src/registry.rs
+++ src/registry.rs
@@ -16,98 +16,22 @@
 // under the License.
 
 //! ObjectStoreRegistry holds all the object stores at Runtime with a scheme for each store.
-//! This allows the user to extend DataFusion with different storage systems such as S3 or HDFS
-//! and query data inside these systems.
+//! This allows the user to resolve a URL to an ObjectStore at runtime. Unlike
+//! [`object_store::parse:parse_url`], this allows for custom logic to be executed
+//! when a URL is resolved to an ObjectStore. It also serves as a cache for object stores
+//! to avoid repeated creation.
 
 use dashmap::DashMap;
-use datafusion_common::{exec_err, DataFusionError, Result};
 #[cfg(not(target_arch = "wasm32"))]
-use object_store::local::LocalFileSystem;
-use object_store::ObjectStore;
+use crate::local::LocalFileSystem;
+use crate::ObjectStore;
 use std::sync::Arc;
 use url::Url;
 
-/// A parsed URL identifying a particular [`ObjectStore`] instance
-///
-/// For example:
-/// * `file://` for local file system
-/// * `s3://bucket` for AWS S3 bucket
-/// * `oss://bucket` for Aliyun OSS bucket
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct ObjectStoreUrl {
-    url: Url,
-}
-
-impl ObjectStoreUrl {
-    /// Parse an [`ObjectStoreUrl`] from a string
-    ///
-    /// # Example
-    /// ```
-    /// # use url::Url;
-    /// # use datafusion_execution::object_store::ObjectStoreUrl;
-    /// let object_store_url = ObjectStoreUrl::parse("s3://bucket").unwrap();
-    /// assert_eq!(object_store_url.as_str(), "s3://bucket/");
-    /// // can also access the underlying `Url`
-    /// let url: &Url = object_store_url.as_ref();
-    /// assert_eq!(url.scheme(), "s3");
-    /// assert_eq!(url.host_str(), Some("bucket"));
-    /// assert_eq!(url.path(), "/");
-    /// ```
-    pub fn parse(s: impl AsRef<str>) -> Result<Self> {
-        let mut parsed =
-            Url::parse(s.as_ref()).map_err(|e| DataFusionError::External(Box::new(e)))?;
-
-        let remaining = &parsed[url::Position::BeforePath..];
-        if !remaining.is_empty() && remaining != "/" {
-            return exec_err!(
-                "ObjectStoreUrl must only contain scheme and authority, got: {remaining}"
-            );
-        }
-
-        // Always set path for consistency
-        parsed.set_path("/");
-        Ok(Self { url: parsed })
-    }
-
-    /// An [`ObjectStoreUrl`] for the local filesystem (`file://`)
-    ///
-    /// # Example
-    /// ```
-    /// # use datafusion_execution::object_store::ObjectStoreUrl;
-    /// let local_fs = ObjectStoreUrl::parse("file://").unwrap();
-    /// assert_eq!(local_fs, ObjectStoreUrl::local_filesystem())
-    /// ```
-    pub fn local_filesystem() -> Self {
-        Self::parse("file://").unwrap()
-    }
-
-    /// Returns this [`ObjectStoreUrl`] as a string
-    pub fn as_str(&self) -> &str {
-        self.as_ref()
-    }
-}
-
-impl AsRef<str> for ObjectStoreUrl {
-    fn as_ref(&self) -> &str {
-        self.url.as_ref()
-    }
-}
-
-impl AsRef<Url> for ObjectStoreUrl {
-    fn as_ref(&self) -> &Url {
-        &self.url
-    }
-}
-
-impl std::fmt::Display for ObjectStoreUrl {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        self.as_str().fmt(f)
-    }
-}
 
 /// [`ObjectStoreRegistry`] maps a URL to an [`ObjectStore`] instance,
-/// and allows DataFusion to read from different [`ObjectStore`]
-/// instances. For example DataFusion might be configured so that
+/// and allows users to read from different [`ObjectStore`]
+/// instances. For example the registry might be configured so that
 ///
 /// 1. `s3://my_bucket/lineitem/` mapped to the `/lineitem` path on an
 ///    AWS S3 object store bound to `my_bucket`
@@ -115,13 +39,6 @@ impl std::fmt::Display for ObjectStoreUrl {
 /// 2. `s3://my_other_bucket/lineitem/` mapped to the (same)
 ///    `/lineitem` path on a *different* AWS S3 object store bound to
 ///    `my_other_bucket`
-///
-/// When given a [`ListingTableUrl`], DataFusion tries to find an
-/// appropriate [`ObjectStore`]. For example
-///
-/// ```sql
-/// create external table unicorns stored as parquet location 's3://my_bucket/lineitem/';
-/// ```
 ///
 /// In this particular case, the url `s3://my_bucket/lineitem/` will be provided to
 /// [`ObjectStoreRegistry::get_store`] and one of three things will happen:
@@ -132,7 +49,7 @@ impl std::fmt::Display for ObjectStoreUrl {
 /// - If an AWS S3 object store can be ad-hoc discovered by the url `s3://my_bucket/lineitem/`, this
 ///   object store will be registered with key `s3://my_bucket` and returned.
 ///
-/// - Otherwise an error will be returned, indicating that no suitable [`ObjectStore`] could
+/// - Otherwise `None` will be returned, indicating that no suitable [`ObjectStore`] could
 ///   be found
 ///
 /// This allows for two different use-cases:
@@ -143,8 +60,6 @@ impl std::fmt::Display for ObjectStoreUrl {
 /// 2. Systems relying on ad-hoc discovery, without corresponding DDL, can create [`ObjectStore`]
 ///    lazily by providing a custom implementation of [`ObjectStoreRegistry`]
 ///
-/// <!-- is in a different crate so normal rustdoc links don't work -->
-/// [`ListingTableUrl`]: https://docs.rs/datafusion/latest/datafusion/datasource/listing/struct.ListingTableUrl.html
 /// [`ObjectStore`]: object_store::ObjectStore
 pub trait ObjectStoreRegistry: Send + Sync + std::fmt::Debug + 'static {
     /// If a store with the same key existed before, it is replaced and returned
@@ -163,7 +78,7 @@ pub trait ObjectStoreRegistry: Send + Sync + std::fmt::Debug + 'static {
     /// If no [`ObjectStore`] found for the `url`, ad-hoc discovery may be executed depending on
     /// the `url` and [`ObjectStoreRegistry`] implementation. An [`ObjectStore`] may be lazily
     /// created and registered.
-    fn get_store(&self, url: &Url) -> Result<Arc<dyn ObjectStore>>;
+    fn get_store(&self, url: &Url) -> Option<Arc<dyn ObjectStore>>;
 }
 
 /// The default [`ObjectStoreRegistry`]
@@ -173,7 +88,7 @@ pub struct DefaultObjectStoreRegistry {
 }
 
 impl std::fmt::Debug for DefaultObjectStoreRegistry {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("DefaultObjectStoreRegistry")
             .field(
                 "schemes",
@@ -230,16 +145,9 @@ impl ObjectStoreRegistry for DefaultObjectStoreRegistry {
         self.object_stores.insert(s, store)
     }
 
-    fn get_store(&self, url: &Url) -> Result<Arc<dyn ObjectStore>> {
+    fn get_store(&self, url: &Url) -> Option<Arc<dyn ObjectStore>> {
         let s = get_url_key(url);
-        self.object_stores
-            .get(&s)
-            .map(|o| Arc::clone(o.value()))
-            .ok_or_else(|| {
-                DataFusionError::Internal(format!(
-                    "No suitable object store found for {url}. See `RuntimeEnv::register_object_store`"
-                ))
-            })
+        self.object_stores.get(&s).map(|o| Arc::clone(o.value()))
     }
 }
 
@@ -258,45 +166,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_object_store_url() {
-        let file = ObjectStoreUrl::parse("file://").unwrap();
-        assert_eq!(file.as_str(), "file:///");
-
-        let url = ObjectStoreUrl::parse("s3://bucket").unwrap();
-        assert_eq!(url.as_str(), "s3://bucket/");
-
-        let url = ObjectStoreUrl::parse("s3://username:password@host:123").unwrap();
-        assert_eq!(url.as_str(), "s3://username:password@host:123/");
-
-        let err = ObjectStoreUrl::parse("s3://bucket:invalid").unwrap_err();
-        assert_eq!(err.strip_backtrace(), "External error: invalid port number");
-
-        let err = ObjectStoreUrl::parse("s3://bucket?").unwrap_err();
-        assert_eq!(err.strip_backtrace(), "Execution error: ObjectStoreUrl must only contain scheme and authority, got: ?");
-
-        let err = ObjectStoreUrl::parse("s3://bucket?foo=bar").unwrap_err();
-        assert_eq!(err.strip_backtrace(), "Execution error: ObjectStoreUrl must only contain scheme and authority, got: ?foo=bar");
-
-        let err = ObjectStoreUrl::parse("s3://host:123/foo").unwrap_err();
-        assert_eq!(err.strip_backtrace(), "Execution error: ObjectStoreUrl must only contain scheme and authority, got: /foo");
-
-        let err =
-            ObjectStoreUrl::parse("s3://username:password@host:123/foo").unwrap_err();
-        assert_eq!(err.strip_backtrace(), "Execution error: ObjectStoreUrl must only contain scheme and authority, got: /foo");
-    }
-
-    #[test]
     fn test_get_url_key() {
-        let file = ObjectStoreUrl::parse("file://").unwrap();
-        let key = get_url_key(&file.url);
+        let file = Url::parse("file://").unwrap();
+        let key = get_url_key(&file);
         assert_eq!(key.as_str(), "file://");
 
-        let url = ObjectStoreUrl::parse("s3://bucket").unwrap();
-        let key = get_url_key(&url.url);
+        let url = Url::parse("s3://bucket").unwrap();
+        let key = get_url_key(&url);
         assert_eq!(key.as_str(), "s3://bucket");
 
-        let url = ObjectStoreUrl::parse("s3://username:password@host:123").unwrap();
-        let key = get_url_key(&url.url);
+        let url = Url::parse("s3://username:password@host:123").unwrap();
+        let key = get_url_key(&url);
         assert_eq!(key.as_str(), "s3://host:123");
     }
 }
