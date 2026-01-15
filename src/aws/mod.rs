dbd4724d07ab6131e03c798a2b9df6bc9a42c48d
james-rms <james.r.m.smith@gmail.com>
2026-01-15 20:08:22 +1100

    Apply suggestion from @tustvold
    Co-authored-by: Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>

--- src/aws/mod.rs
+++ src/aws/mod.rs
@@ -662,7 +662,7 @@ mod tests {
         let store = AmazonS3Builder::from_env()
             .with_bucket_name(bucket)
             .with_multipart_copy_threshold(5 * 1024 * 1024)
-            .with_multipart_copy_part_size(5 * 1024 * 1024)
+            .with_multipart_copy_part_size(1337 * 1024)
             .build()
             .unwrap();
 
