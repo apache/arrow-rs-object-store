c7316d29face118e7409eead0cda098f38589428
Andrew Lamb <andrew@nerdnetworks.org>
2026-07-10 10:03:16 -0400

    Update version to 0.14.1 and add CHANGELOG (#794)
    * Update version to 0.14.1 and add CHANGELOG
    
    * update
    
    * Updates
    
    * Restore 0.14.0 changelog

--- dev/release/update_change_log.sh
+++ dev/release/update_change_log.sh
@@ -29,8 +29,8 @@
 
 set -e
 
-SINCE_TAG="v0.13.2"
-FUTURE_RELEASE="v0.14.0"
+SINCE_TAG="v0.14.0"
+FUTURE_RELEASE="v0.14.1"
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_TOP_DIR="$(cd "${SOURCE_DIR}/../../" && pwd)"
